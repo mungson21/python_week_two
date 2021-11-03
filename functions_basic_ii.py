5883bd4320db12bb991c4a2d6137bd6697c87624
m ungs <ungson001@live.com>
2021-11-03 16:55:51 -0700

    challenge #1 complete

---
+++ functions_basic_ii.py
@@ -0,0 +1,12 @@
+# Countdown - Create a function that accepts a number as an input. 
+# Return a new list that counts down by one, from the number (as the 0th element) 
+# down to 0 (as the last element).
+# Example: countdown(5) should return [5,4,3,2,1,0]
+
+def countDown(input):
+    print(input)
+    while input > 0:
+        input -= 1
+        print(input)
+
+countDown(6)
